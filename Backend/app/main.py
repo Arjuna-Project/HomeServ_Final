47abc05668100ebb3d461e68a196c75b5a64ee83
Arjuna-Project <arjunvk1819@gmail.com>
2026-01-05 18:49:30 +0530

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="HomeServ API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://home-serv-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
