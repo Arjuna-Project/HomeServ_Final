d05722ccbff7d1c94a79b641b4fae836acfcbb59
Arjuna-Project <arjunvk1819@gmail.com>
2026-01-05 14:50:12 +0530

    Updated main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth
+from app.routers import (
+    auth,
+    user,
+    area,
+    service,
+    professionals,
+    packages,
+    bookings,
+    contact,
+    professional_auth,
+    professional_dashboard,
+    professional_jobs)
 
 app = FastAPI(title="HomeServ API")
 
@@ -13,100 +24,17 @@ app.add_middleware(
 )
 
 app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(area.router)
+app.include_router(service.router)
+app.include_router(professionals.router)
+app.include_router(packages.router)
+app.include_router(bookings.router)
+app.include_router(contact.router)
+app.include_router(professional_auth.router)
+app.include_router(professional_dashboard.router)
+app.include_router(professional_jobs.router)
 
 @app.get("/")
 def root():
     return {"message": "Backend alive"}
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-# from app.routers import (
-#     auth,
-#     user,
-#     area,
-#     service,
-#     professionals,
-#     packages,
-#     bookings,
-#     contact,
-#     professional_auth,
-#     professional_dashboard,
-#     professional_jobs)
-# # from dotenv import load_dotenv
-# load_dotenv()   # ✅ loads .env (local) or Vercel env vars
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-# from app.core.database import Base, engine
-# from app.routers import professional_auth, professional_dashboard, professional_jobs
-# from app.routers import (
-#     auth,
-#     user,
-#     area,
-#     service,
-#     professionals,
-#     packages,
-#     bookings,
-#     contact
-# )
-
-# Base.metadata.create_all(bind=engine)
-
-# app = FastAPI(title="HomeServ API", version="1.0.0")
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["https://teal-sfogliatella-1fc33b.netlify.app/"],   # tighten later for production
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(auth.router)
-# app.include_router(user.router)
-# app.include_router(area.router)
-# app.include_router(service.router)
-# app.include_router(professionals.router)
-# app.include_router(packages.router)
-# app.include_router(bookings.router)
-# app.include_router(contact.router)
-# app.include_router(professional_auth.router)
-# app.include_router(professional_dashboard.router)
-# app.include_router(professional_jobs.router)
-
-# @app.get("/")
-# def root():
-#     return {"message": "HomeServ Backend Running 🚀"}
-
-# app = FastAPI(title="HomeServ API", version="1.0.0")
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         "https://teal-sfogliatella-1fc33b.netlify.app"
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# # Routers
-# app.include_router(auth.router)
-# app.include_router(user.router)
-# app.include_router(area.router)
-# app.include_router(service.router)
-# app.include_router(professionals.router)
-# app.include_router(packages.router)
-# app.include_router(bookings.router)
-# app.include_router(contact.router)
-# app.include_router(professional_auth.router)
-# app.include_router(professional_dashboard.router)
-# app.include_router(professional_jobs.router)
-
-# @app.get("/")
-# def root():
-#     return {"message": "HomeServ Backend Running 🚀"}
-
-
