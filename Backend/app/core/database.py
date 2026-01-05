45f17de98bee4830b3a823a21cc26cdc99389172
Arjuna-Project <arjunvk1819@gmail.com>
2026-01-05 14:21:03 +0530

    Change db.py nd main.py

--- Backend/app/core/database.py
+++ Backend/app/core/database.py
@@ -1,9 +1,38 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# from dotenv import load_dotenv
 
-load_dotenv()  # works locally & harmless on Vercel
+# load_dotenv()  # works locally & harmless on Vercel
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# if not DATABASE_URL:
+#     raise RuntimeError("DATABASE_URL is not set")
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     pool_pre_ping=True
+# )
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+# # Dependency
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -30,3 +59,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
